85e9deeb209963e0dad593f0edc33eff221a8d53
if23b118 <if23b118@technikum-wien.at>
2024-12-06 00:25:48 +0100

    added rabbitmq

--- src/main/java/org/technikum/dms/document/DocumentController.java
+++ src/main/java/org/technikum/dms/document/DocumentController.java
@@ -3,6 +3,7 @@ package org.technikum.dms.document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.technikum.dms.services.RabbitMQSender;
 
 @RestController
 @RequestMapping("/api/documents")
@@ -12,10 +13,19 @@ public class DocumentController {
     @Autowired
     private DocumentService documentService;
 
+    @Autowired
+    private RabbitMQSender rabbitMQSender;
+
     @PostMapping
     public ResponseEntity<String> uploadDocument(@RequestBody DocumentDTO documentDTO) {
         Document savedDocument = documentService.saveDocument(documentDTO);
 
         return ResponseEntity.ok("Document uploaded successfully with ID: " + savedDocument.getId());
+    }
+
+    @PostMapping("/send-to-queue")
+    public String sendToQueue(@RequestBody String document) {
+        rabbitMQSender.sendMessage(document);
+        return "Document sent to OCR queue";
     }
 }
