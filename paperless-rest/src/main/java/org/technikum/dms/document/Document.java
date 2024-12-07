dc9527d737ef5639bc254ebc333f8c0707300cf2
Philipp Chn <19249044+philippchn@users.noreply.github.com>
2024-12-07 15:38:43 +0100

    refactoring code, removing ngx, implementing nginx

--- paperless-rest/src/main/java/org/technikum/dms/document/Document.java
+++ paperless-rest/src/main/java/org/technikum/dms/document/Document.java
@@ -1,9 +1,6 @@
 package org.technikum.dms.document;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +8,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @Entity
+@Table(name = "t_documents")
 public class Document {
     public Document() {
 
