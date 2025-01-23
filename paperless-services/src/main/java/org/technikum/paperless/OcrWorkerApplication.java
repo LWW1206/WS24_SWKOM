8b2a62a42f8df8af0714cbb5fdc8a97fbe9d9d54
Li <liwen.wang1206@gmail.com>
2025-01-23 11:00:40 +0100

    dockerfile + pom for ocr

--- paperless-services/src/main/java/org/technikum/paperless/OcrWorkerApplication.java
+++ paperless-services/src/main/java/org/technikum/paperless/OcrWorkerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OcrWorkerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(OcrWorkerApplication.class, args);
     }
