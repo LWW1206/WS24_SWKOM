4fd749811cdfc10e72317b10c5c0420c5f7185f6
Li <liwen.wang1206@gmail.com>
2025-01-30 14:16:29 +0100

    fix some name issues.

--- paperless-rest/src/main/java/org/technikum/dms/rabbitmq/IOcrJobProducer.java
+++ paperless-rest/src/main/java/org/technikum/dms/rabbitmq/IOcrJobProducer.java
@@ -1,5 +1,7 @@
 package org.technikum.dms.rabbitmq;
 
+import org.springframework.context.annotation.Bean;
+
 public interface IOcrJobProducer {
     void triggerOcrJob(String documentId, String filename);
 }
